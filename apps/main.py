a7f30b86130bc5cac84ad750d942ad659f63c7bf
Bala chandra Sekhar Reddy Jannavarapu <bj1057@unh.edu>
2022-12-01 00:20:14 -0500

    Initial Commit

---
+++ apps/main.py
@@ -0,0 +1,32 @@
+"""
+Represent a data-set of Exchange_Rates
+
+Authors:
+  - https://github.com/bj1057
+  - https://github.com/sualeh
+
+Implementation requirements:
+   1. MUST use a for ... in ... loop
+   1. MUST NOT use list comprehensions
+   2. MUST NOT use lambda functions
+   3. MUST NOT call built-in functions or methods that
+      eliminate the use of a for loop
+   4. MUST NOT use for ... in ... range(len(...))
+"""
+import sys
+
+
+class ExchangeRates:
+    """
+    Represent a data-set of Exchange Rates Information of different countries.
+
+    Concepts:
+        Data processing functionality.
+        Reading and writing from CSV files.
+    """
+    def __int__(self,exchange_rate_info: ):
+        """
+
+        :param exchange_rate_info:
+        :return:
+        """
