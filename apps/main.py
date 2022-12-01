39c6fee1348fb5af633d993cdcbe1621b810449a
Bala chandra Sekhar Reddy Jannavarapu <bj1057@unh.edu>
2022-12-01 02:45:36 -0500

    Added Proposal document

--- apps/main.py
+++ apps/main.py
@@ -3,7 +3,7 @@ Represent a data-set of Exchange_Rates
 
 Authors:
   - https://github.com/bj1057
-  - https://github.com/sualeh
+  - https://github.com/ns1270
 
 Implementation requirements:
    1. MUST use a for ... in ... loop
@@ -24,9 +24,81 @@ class ExchangeRates:
         Data processing functionality.
         Reading and writing from CSV files.
     """
-    def __int__(self,exchange_rate_info: ):
+    def __int__(self, exchange_rate_info: list):
+        """
+        Create and initialize the class attributes.
+
+        Attributes: a list of tuples of:
+            exchange_rate: integer
+            date: string
+            currency_name: 3-character string
+        """
+        self.exchange_rate_info = exchange_rate_info
+
+    def write(self, filename: str):
+        """
+        Write a CSV file with exchange rates data.
+
+        A record is a row in the file, with three columns, corresponding to
+        date,exchange rate,exchange currency
+
+        filename: filename to write data to
         """
 
-        :param exchange_rate_info:
-        :return:
+    def exchange_rates_country(self) -> dict:
         """
+        Getting Information of exchange rate of Indian Rupee and US dollar based upon the particular date.
+
+        Return: nested dictionary with currency exchange rate values of Indian Rupee and US dollar based upon the particular date.
+        Dictionary
+             keys: string, each representing a particular date
+             values:Nested dictionary of currency_name,currency_value
+        Nested Dictionary
+            keys: string, each representing a Currency name
+            values:integer, currency values of that particular country
+        """
+
+    def exchange_rates_range_of_dates (self) -> list:
+        """
+        Create a lookup of exchange rates of each country in range of date
+
+        Return: List of dictionaries, with
+            keys: string, each representing a Currency name
+            values: currency values of that particular country
+        """
+
+    def exchangerate_on_date(self) -> dict:
+        """
+        Create a lookup of .
+
+        Return: nested dictionary with currency exchange rate values based upon the date.
+        Dictionary
+             keys: string, each representing a particular date
+             values:Nested dictionary of currency_name,currency_value
+        Nested Dictionary
+            keys: string, each representing a Currency name
+            values:integer, currency values of that particular country
+        """
+
+    def read_dataset(filename: str) -> ExchangeRates:
+        """
+        Read a CSV text file that holds 3-element records,where each has name.
+
+        currency_name, and currency_value of exchange rates.
+        Need to create an empty list.
+        Use the file open method with reading mode.
+
+        Return the list of tuples of exchangerates.
+        """
+
+
+def main():
+    """
+    Run the sample code.
+
+    (Tests are in different modules.)
+    """
+
+
+if __name__ == '__main__':
+    main()
